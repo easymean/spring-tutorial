81e18d4c80d9e4c68c034359859d3ffe195de7c7
easymean <easymean@ncsoft.com>
2023-03-16 17:03:21 +0900

    code reformat

--- src/test/java/hello/hellospring/respository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/respository/MemoryMemberRepositoryTest.java
@@ -1,29 +1,29 @@
 package hello.hellospring.respository;
 
 import hello.hellospring.domain.Member;
-import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
+// 테스트는 서로 의존관계를 가지지 않아야 한다.
+// 테스트 실행 순서와 상관없이 헝성 동일한 테스트 결과가 나와야 한다.
 class MemoryMemberRepositoryTest {
 
     MemoryMemberRepository repository = new MemoryMemberRepository();
 
-    // 테스트 한 개가 종료될 떄마다 실행되는
+    // 테스트 한 개가 종료될 떄마다 repository를 비워준다.
     @AfterEach
-    public void afterEach(){
-repository.clearStore();
+    public void afterEach() {
+        repository.clearStore();
     }
 
     @Test
-    public void save(){
+    public void save() {
         Member member = new Member();
         member.setName("Spring");
         repository.save(member);
@@ -35,7 +35,7 @@ repository.clearStore();
     }
 
     @Test
-    public void findByName(){
+    public void findByName() {
         Member member1 = new Member();
         member1.setName("hello");
         repository.save(member1);
@@ -50,7 +50,7 @@ repository.clearStore();
     }
 
     @Test
-    public void findAll(){
+    public void findAll() {
         Member member1 = new Member();
         member1.setName("spring1");
         repository.save(member1);
